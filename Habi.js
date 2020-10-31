f47953d0d53515c0419375bd90f2331245c9c334
Glitch (project-wave) <none>
2020-10-31 00:49:02 +0000

    ❤️🏠 Checkpoint ./Habi.js:23303697/270 ./hmboka_App.html:23303697/1573

--- Habi.js
+++ Habi.js
@@ -1,6 +1,2 @@
-var a = 2 + 3;
-let b = Math.floor(Math.random(0) * 10 + 1);
-
-var ball = 3 + 4;
-document.getElementById("first").innerHTML = ball;
-document.getElementsByTagName("first")[0].innerHTML = ball;
+      var val1 = (5+2);
+      document.getElementById("first").innerHTML = val1;
