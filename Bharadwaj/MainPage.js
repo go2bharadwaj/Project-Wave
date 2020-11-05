8dc615d21d596a0c54aa41f26f2c86fe676182f3
Glitch (project-wave) <none>
2020-11-05 22:16:44 +0000

    👣🏊🏼 Checkpoint ./Bharadwaj/Bharadwaj.js:23303666/6 ./Bharadwaj/MainPage.html:23303666/41 ./Bharadwaj/Bharadwaj.css:23303666/1006 ./Bharadwaj/MainPage.js:23303666/360 ./Bharadwaj/App.html:23303666/107

--- Bharadwaj/MainPage.js
+++ Bharadwaj/MainPage.js
@@ -1,3 +1,6 @@
+let question = "=============================" //string for question
+document.getElementById("background").innerHTML = question;
+
 function changeColor(newColor) {
   var elem = document.getElementById('para');
   elem.style.color = newColor;
