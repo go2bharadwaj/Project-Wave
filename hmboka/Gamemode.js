e746b83c54561f68f6ab8db7a519d8dbbedca566
Glitch (project-wave) <none>
2020-11-14 21:59:46 +0000

    ❄️📷 Checkpoint ./hmboka/mainpg.js:23303697/275 ./hmboka/Gamemode.html:23303697/1252 ./hmboka/Gamemode.js:23303697/267

---
+++ hmboka/Gamemode.js
@@ -0,0 +1,9 @@
+var cars = [];
+document.getElementById("MainHeader").innerHTML = "Select Game Mode";
+document.getElementById("buttonBegin").innerHTML = "Click to Begin";
+
+
+function changeColor(newColor) {
+  var elem = document.getElementById('para');
+  elem.style.color = newColor;
+}
---
+++ hmboka/mainpg.js
@@ -0,0 +1,9 @@
+var cars = [];
+document.getElementById("MainHeader").innerHTML = "Welcome to Project Wave!";
+document.getElementById("buttonBegin").innerHTML = "Click to Begin";
+
+
+function changeColor(newColor) {
+  var elem = document.getElementById('para');
+  elem.style.color = newColor;
+}
