f8d4aa185791d0f421f65c8bbe47ed0308a6ddf1
Glitch (project-wave) <none>
2020-11-24 07:22:20 +0000

    🐧🎄 Checkpoint ./hmboka/App.js:23303697/57041

--- hmboka/App.js
+++ hmboka/App.js
@@ -1,220 +1,151 @@
 var total = 0;
 var problemNo = 0;
 var progID = "";
-var testingVar;
 game();
 
-function game() {
-  document.addEventListener("keydown", function(event) {
-    if (problemNo !== gameMode) {
-      if (event.keyCode == 37) {
-        LeftSelected();
-        LoadTotal(total);
-        problemNo = problemNo + 1;
-        game();
-        console.log("left swipe detected");
-        console.log(problemNo);
-      } else if (event.keyCode == 39) {
-        RightSelected();
-        LoadTotal(total);
-        problemNo = problemNo + 1;
-        game();
-        console.log("right swipe detected");
-        console.log(problemNo);
-      }
-    } else {
-      LoadEnd();
-    }
-  });
-  let num1 = generateRandom(1, 5, -1); //creates number 0 to 5 for first num in equation
-  let num2 = generateRandom(1, 5, -1); //creates number 0 to 5 for second num in equation
-  let correct_num = LoadCorrect_num(num1, num2); //correct answer
-  let incorrect_num = generateRandom(0, 10, correct_num); //glitch not loading test tomorrow prevents correct answer twice, exclude the correct answer
-  var combo = 0;
-  var boolMakeYellow = true;
-  selectProgID();
-  makeYellow();
 
-  LoadHeader();
-  LoadOptions(correct_num, incorrect_num);
-  LoadTotal(total);
-  //LoadAnswerCol();
+function game(){  
+            
+            let num1 = generateRandom(1, 5, -1);//creates number 0 to 5 for first num in equation
+            let num2 = generateRandom(1, 5, -1);//creates number 0 to 5 for second num in equation
+            let correct_num = LoadCorrect_num(num1,num2); //correct answer
+            let incorrect_num = generateRandom(0, 10, correct_num); //glitch not loading test tomorrow prevents correct answer twice, exclude the correct answer
+            var combo = 0;
+            progID = getProgID();
+            
+            LoadHeader();
+            LoadOptions(correct_num,incorrect_num);
+            LoadTotal(total);
+            //LoadAnswerCol();
+           
 
-  //Helper Functions_______________________________________________________________________________________________________________________________________________________________________________
-  //______________________________________________________________________________________________________________________________________________________________________________________________
-  function LeftSelected() {
-    boolMakeYellow = false;
-    if (combo === 1) {
-      testingVar = document.getElementById(progID);
-      testingVar.style.background = "green";
-      var elem1 = document.getElementById("first");
-      elem1.style.color = "green";
-      var elem2 = document.getElementById("second");
-      elem2.style.color = "white";
-      total = total + 1;
-    } else if (combo === 2) {
-      testingVar = document.getElementById(progID);
-      testingVar.style.background = "red";
-      var elem1 = document.getElementById("first");
-      elem1.style.color = "red";
-      var elem2 = document.getElementById("second");
-      elem2.style.color = "white";
-    }
-  }
 
-  function RightSelected() {
-    if (combo === 1) {
-      testingVar = document.getElementById(progID);
-      testingVar.style.background = "red";
-      var elem1 = document.getElementById("first");
-      elem1.style.color = "white";
-      var elem2 = document.getElementById("second");
-      elem2.style.color = "red";
-    } else if (combo === 2) {
-      testingVar = document.getElementById(progID);
-      testingVar.style.background = "green";
-      var elem1 = document.getElementById("first");
-      elem1.style.color = "white";
-      var elem2 = document.getElementById("second");
-      elem2.style.color = "green";
-      total = total + 1;
-    }
-  }
-  function selectProgID() {
-    if (problemNo == 1) {
-      progID = "pgcirctrying";
-      // testingVar = document.getElementById(progID);
-      // if(makeYellow == true){
-      //   testingVar.style.background = "yellow";
-      // }
-    console.log("IN SELECTPROGID");
-    } else if (problemNo == 2) {
-      progID = "pgc1";
-    } else if (problemNo == 3) {
-      progID = "pgc1";
-    } else if (problemNo == 4) {
-      progID = "pgc1";
-    } else if (problemNo == 5) {
-      progID = "pgc1";
-    } else if (problemNo == 5) {
-      progID = "pgc1";
-    } else if (problemNo == 6) {
-      progID = "pgc1";
-    } else if (problemNo == 7) {
-      progID = "pgc1";
-    } else if (problemNo == 8) {
-      progID = "pgc1";
-    } else if (problemNo == 9) {
-      progID = "pgc1";
-    } else if (problemNo == 10) {
-      progID = "pgc1";
-    } else if (problemNo == 11) {
-      progID = "pgc1";
-    } else if (problemNo == 12) {
-      progID = "pgc1";
-    } else if (problemNo == 13) {
-      progID = "pgc1";
-    } else if (problemNo == 14) {
-      progID = "pgc1";
-    } else {
-      progID = "pgc1";
-    }
-  }
+//Helper Functions_______________________________________________________________________________________________________________________________________________________________________________
+//______________________________________________________________________________________________________________________________________________________________________________________________
+            function LeftSelected(){
+              if(combo === 1){
+                var elem1 = document.getElementById("first");
+                elem1.style.color = 'green';
+                var elem2 = document.getElementById("second");
+                elem2.style.color = 'white';
+                total = total + 1;
+              }
+              else if(combo === 2){
+                var elem1 = document.getElementById("first");
+                elem1.style.color = 'red';
+                var elem2 = document.getElementById("second");
+                elem2.style.color = 'white';
+              }
+            }
 
-  function makeYellow() {
-    console.log("IN MAKEYELLOW ", boolMakeYellow);
+            function RightSelected(){
+              if(combo === 1){
+                var elem1 = document.getElementById("first");
+                elem1.style.color = 'white';
+                var elem2 = document.getElementById("second");
+                elem2.style.color = 'red';
+              }
+              else if(combo === 2){
+                var elem1 = document.getElementById("first");
+                elem1.style.color = 'white';
+                var elem2 = document.getElementById("second");
+                elem2.style.color = 'green';
+                total = total + 1;
+              }
+            }
+          
+            
+            function MakeWhite(){
+               var elem1 = document.getElementById("first");
+               elem1.style.color = 'white';
+               var elem2 = document.getElementById("second");
+               elem2.style.color = 'white';
+            }
+            
+            function LoadHeader(){
+              // add animation
+              document.getElementById("header1").innerHTML = "<- Swipe ->";
+              var elem1 = document.getElementById("header1");
+              elem1.style.color = 'black';
+            }
 
-    testingVar = document.getElementById(progID);
-    if (boolMakeYellow == true) {
-      testingVar.style.background = "yellow";
-    }
-  }
 
-  function MakeWhite() {
-    var elem1 = document.getElementById("first");
-    elem1.style.color = "white";
-    var elem2 = document.getElementById("second");
-    elem2.style.color = "white";
-  }
+            function generateRandom(min, max, exclude){
+                var num = Math.floor(Math.random() * (max - min + 1)) + min;
+                return (num === exclude) ? generateRandom(min, max, exclude) : num;
+            }
 
-  function LoadHeader() {
-    // add animation
-    document.getElementById("header1").innerHTML = "<- Swipe ->";
-    var elem1 = document.getElementById("header1");
-    elem1.style.color = "black";
-  }
+            function LoadTotal(total){ 
+                  document.getElementById("third").innerHTML = total;
+            }
 
-  function generateRandom(min, max, exclude) {
-    var num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num === exclude ? generateRandom(min, max, exclude) : num;
-  }
+            function LoadAdditionQuestion(num1,num2){
+                  let question = num1 + " + " + num2 + " = "; //string for question
+                  document.getElementById("header2").innerHTML = question;
+            }
+  
+            function LoadSubtractionQuestion(num1,num2){
+                  let question = num1 + " - " + num2 + " = "; //string for question
+                  document.getElementById("header2").innerHTML = question;
+            }
 
-  function LoadTotal(total) {
-    document.getElementById("third").innerHTML = total;
-  }
+            function LoadOptions(correct_num,incorrect_num){
+                let decider = generateRandom(0, 2, -1);   
+                  if(decider === 0){
+                    document.getElementById("first").innerHTML = correct_num;
+                    document.getElementById("second").innerHTML = incorrect_num;
+                    combo = 1;
+                  }
+                  else{
+                    document.getElementById("second").innerHTML = correct_num;
+                    document.getElementById("first").innerHTML = incorrect_num;
+                    combo = 2;
+                  }
+            }
+            
+            function LoadAnswerCol(){
+              
+            }
+            
+            function LoadCorrect_num(num1, num2){
+              let decider = generateRandom(0, 2, -1);
+              let number;
+                  if(decider === 0){
+                     number = num1 + num2;
+                     LoadAdditionQuestion(num1,num2);
+                  }
+                  else{
+                     number = num1 - num2;
+                     LoadSubtractionQuestion(num1,num2);
+                  }
+              return number;
+            }
+            
+            function LoadEnd(){
+              document.getElementById("header2").innerHTML = "Total!";
+              document.getElementById("first").innerHTML = "->";
+              document.getElementById("second").innerHTML = "<-";
+              var elem1 = document.getElementById("first");
+              elem1.style.color = 'gold';
+              var elem2 = document.getElementById("second");
+              elem2.style.color = 'gold';
+            }
+  
+  
+            function move() {
+                var elem = document.getElementById("myBar");   
+                var width = 1;
+                var id = setInterval(frame, 10);
+                function frame() {
+                  if (width >= 100) {
+                    clearInterval(id);
+                  } else {
+                    width++; 
+                    elem.style.width = width + '%'; 
+                  }
+                }
+              }
 
-  function LoadAdditionQuestion(num1, num2) {
-    let question = num1 + " + " + num2 + " = "; //string for question
-    document.getElementById("header2").innerHTML = question;
-  }
-
-  function LoadSubtractionQuestion(num1, num2) {
-    let question = num1 + " - " + num2 + " = "; //string for question
-    document.getElementById("header2").innerHTML = question;
-  }
-
-  function LoadOptions(correct_num, incorrect_num) {
-    let decider = generateRandom(0, 2, -1);
-    if (decider === 0) {
-      document.getElementById("first").innerHTML = correct_num;
-      document.getElementById("second").innerHTML = incorrect_num;
-      combo = 1;
-    } else {
-      document.getElementById("second").innerHTML = correct_num;
-      document.getElementById("first").innerHTML = incorrect_num;
-      combo = 2;
-    }
-  }
-
-  function LoadAnswerCol() {}
-
-  function LoadCorrect_num(num1, num2) {
-    let decider = generateRandom(0, 2, -1);
-    let number;
-    if (decider === 0) {
-      number = num1 + num2;
-      LoadAdditionQuestion(num1, num2);
-    } else {
-      number = num1 - num2;
-      LoadSubtractionQuestion(num1, num2);
-    }
-    return number;
-  }
-
-  function LoadEnd() {
-    document.getElementById("header2").innerHTML = "Total!";
-    document.getElementById("first").innerHTML = "->";
-    document.getElementById("second").innerHTML = "<-";
-    var elem1 = document.getElementById("first");
-    elem1.style.color = "gold";
-    var elem2 = document.getElementById("second");
-    elem2.style.color = "gold";
-  }
-
-  function move() {
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
-    }
-  }
-  /*
 //SOLI___________________________________________________________________________________________________________________________________________________________
             window.onSoliEvent = function(event) { // this function will run any time a gesture is detected'
               if(problemNo !== gameMode){
@@ -257,5 +188,10 @@ function game() {
             //_________________________________________________________________________________________________________________________________________________________________
             //_________________________________________________________________________________________________________________________________________________________________________________________________
             //_________________________________________________________________________________________________________________________________________________________________________________________________
-  */
-}
+  } 
+      
+   
+
+
+
+
