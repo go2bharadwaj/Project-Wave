97038fd14255e86b5a4b0a24b24e3056f6d6ec5c
Glitch (project-wave) <none>
2020-11-25 01:51:10 +0000

    🍧🌝 Checkpoint ./hmboka/App_5.html:23303697/25726 ./hmboka/App.css:23303697/1662 ./hmboka/App.js:23303697/201

--- hmboka/App.js
+++ hmboka/App.js
@@ -7,6 +7,13 @@ game();
 
 
 function game(){  
+  onclick="Alert.ok()">OK</button>';
+    }
+	this.ok = function(){
+		document.getElementById('dialogbox').style.display = "none";
+		document.getElementById('dialogoverlay').style.display = "none";
+	}
+}
               document.addEventListener("keydown", function(event) {
               
               if (problemNo <= gameMode) {
